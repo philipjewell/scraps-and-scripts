06a174c14f1da62139c99bec5bf1932942ab4003
Philip Jewell <philip@wpengine.com>
2017-08-04 11:41:56 -0500

    addition of the php function check

---
+++ function_check.php
@@ -0,0 +1,7 @@
+<?php
+if (function_exists('FUNCTION_HERE')) {
+    echo "FUNCTION_HERE functions are available.<br />\n";
+} else {
+    echo "FUNCTION_HERE functions are not available.<br />\n";
+}
+?>
